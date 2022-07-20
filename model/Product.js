32e6b0506bd042cfd9edff773b74b8e9ba73389c
hieunguyen <hieumnguyen1810@gmail.com>
2022-07-20 15:27:48 +0700

    MVC model

---
+++ model/Product.js
@@ -0,0 +1,15 @@
+function Product() {
+    this.id = "";
+    this.name = "";
+    this.alias = "";
+    this.price = 0;
+    this.description = "";
+    this.size = null;
+    this.shortDescription = "";
+    this.quantity = 0;
+    this.deleted = false;
+    this.categories = null;
+    this.relatedProducts = null;
+    this.feature = false;
+    this.image = "";
+}
